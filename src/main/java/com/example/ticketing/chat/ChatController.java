360bdda8f06d35bfa349fee4f4df104c86d15e08
하지훈 <jnh03336@gmail.com>
2024-10-27 15:28:52 +0900

    fix: 파일 위치 수정...

--- src/main/java/com/example/ticketing/chat/ChatController.java
+++ src/main/java/com/example/ticketing/chat/ChatController.java
@@ -1,16 +1,22 @@
-package com.example.chat;
+package com.example.ticketing.chat;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 @RestController
-@RequestMapping(("/chat"))
+@RequestMapping("/chat")
 public class ChatController {
-    @GetMapping("/enter")
-    public String enterChatRoom(){
-        return "enter";
+
+
+    @GetMapping(value = "/enter", produces = "text/event-stream")
+    public Flux<String> enterChatRoom(){
+        return Flux.interval(Duration.ofSeconds(1)) // 1초 간격으로 이벤트 생성
+                .map(seq -> "Event " + seq);
     }
 
     @PostMapping("/send")
