2a5b3d2235e3fb54432fba7d3da09f3d0b15632f
하지훈 <jnh03336@gmail.com>
2024-10-27 15:09:23 +0900

    feat: skeleton controller

---
+++ src/main/java/com/example/ticketing/chat/ChatController.java
@@ -0,0 +1,20 @@
+package com.example.ticketing.chat;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(("/chat"))
+public class ChatController {
+    @GetMapping("/enter")
+    public String enterChatRoom(){
+        return "enter";
+    }
+
+    @PostMapping("/send")
+    public String sendChat(){
+        return "sendChat";
+    }
+}
