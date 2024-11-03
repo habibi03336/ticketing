60b46a5e69ef99d9c019905fa112fe1371dee5f3
하지훈 <jnh03336@gmail.com>
2024-11-03 13:29:45 +0900

    feat: 음 에러 안나도록 하는 코드 추가

--- src/main/java/com/example/ticketing/chat/ChatService.java
+++ src/main/java/com/example/ticketing/chat/ChatService.java
@@ -10,7 +10,9 @@ public class ChatService {
     Sinks.Many<ChatVO> channel = Sinks.many().multicast().directAllOrNothing();
 
     public Flux<ServerSentEvent<ChatVO>> connect() {
-        return channel.asFlux().map((c) -> ServerSentEvent.builder(c).build());
+        return channel.asFlux().map((c) -> ServerSentEvent.builder(c).build()).doOnCancel(()->{
+            channel.asFlux().blockLast(); // 이 doOnCancel을 추가하지 않으면 에러가 발생한다. why?
+        });
     }
 
     public void addChat(ChatVO chat){
